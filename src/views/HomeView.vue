e92183e06d2b68f0a5a53221bc416dcc46f0544d
Iasonas <iasonas.ps@gmail.com>
2025-03-22 12:24:39 +0200

    Initial_22_3

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,24 @@
 <template>
   <div>
-    <h2>Test</h2>
     <GoogleLogin :callback="callback" propmt />
   </div>
 
   <div>
-    <h4>{{ result }}</h4>
+    <!-- <h4>{{ result }}</h4> -->
+    <table>
+      <tbody>
+        <tr v-for="(value, key) in result" :key="key">
+          <td>{{ key }}</td>
+          <td>{{ value }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
-
 </template>
-
 
 <script setup lang="ts">
 import { ref } from 'vue'
 import { decodeCredential } from 'vue3-google-login'
-
 
 const result = ref<object>({})
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
